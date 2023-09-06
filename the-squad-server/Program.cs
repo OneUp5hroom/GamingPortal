81dd1be5566d0f8aa233d9f6d0b8084ce2cfe9ff
Joshua Donovan <Joshua.Donovan@wardwiseman.com>
2023-09-06 16:28:47 -0500

    0

--- the-squad-server/Program.cs
+++ the-squad-server/Program.cs
@@ -11,7 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<UserDbContext>(options =>
-    options.UseSqlite(connectionString));  
+    options.UseSqlServer(connectionString));
+
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
@@ -32,7 +34,11 @@ builder.Services.AddSingleton<APIService<Server>>(provider =>
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));  
+    options.UseSqlite(connectionString));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddScoped<CreatorManager<Creator>>();
 builder.Services.AddScoped<GameManager<Game>>();
 builder.Services.AddScoped<StreamingServiceManager<StreamingService>>();
@@ -52,9 +58,6 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-// Remove for Production
-app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
